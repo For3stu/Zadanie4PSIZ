6274a9efea5194240685314a76ae07a69d93c084
For3stu <70603167+For3stu@users.noreply.github.com>
2022-06-25 13:28:42 +0200

    Add menu

--- Zadanie4/Zadanie4.cpp
+++ Zadanie4/Zadanie4.cpp
@@ -1,10 +1,39 @@
 ﻿#include <iostream>
+#include <string>
 
 using namespace std;
 
+int choice;
 
-int main()
+int main(int argc, char* argv[])
 {
-    cout << "Hello World!\n";
+    if (argc != 3) {
+        do {
+            cout << "Kalkulator 0.1\n";
+            cout << "1. Obliczenie BER" << endl << "2. Test 1" << endl << "3. Test 2" << endl << "4. Test 3" << endl << "0. Quit\n";
+            
+            cin >> choice;
+
+            switch (choice)
+            {
+            case 0:
+                cout << "Quit\n";
+                return 0;
+            case 1:
+                cout << "Obliczenie\n";
+                break;
+            case 2:
+                cout << "Test1\n";
+                break;
+            case 3:
+                cout << "Test2\n";
+                break;
+            case 4:
+                cout << "Test3\n";
+                break;;
+            }
+        } 
+        while (choice !=0);
+    }
 }
 
