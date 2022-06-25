5ea72dcfdcabad3b311971683c28428363b0db57
For3stu <70603167+For3stu@users.noreply.github.com>
2022-06-25 15:01:24 +0200

    Dodanie funkcji tworzeniepliku1/tworzenie i system logówAdd new function "Creating files for Test1, Test2, Test3"

--- Zadanie4/Zadanie4.cpp
+++ Zadanie4/Zadanie4.cpp
@@ -1,20 +1,90 @@
-﻿#include <iostream>
+﻿#pragma warning(disable : 4996)
+#include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
+
 
 using namespace std;
 
-int choice;
+int wybor;
+fstream logFile;
+
+void Zapis_Logow(string msg)
+{
+    string ss;
+    time_t currentTime = time(NULL);
+    string txttime = (string)asctime(localtime(&currentTime));
+    txttime = txttime.substr(0, txttime.length() - 1);
+    ss = (string)"T: " + txttime + " M: " + msg + "\n";
+    logFile << ss.c_str();
+    logFile.flush();
+    cout << ss.c_str();
+    cout.flush();
+}
+
+void Otwarcie_loga(string fileName)
+{
+    logFile.open(fileName.c_str(), ios_base::app);
+    if (!logFile.good() == true)
+    {
+        cout << "Brak pliku log " << "log.txt" << endl;
+    }
+    Zapis_Logow("Log zostal otwarty");
+}
+
+
+void closeLog(void)
+{
+    Zapis_Logow("Log jest zamkniety!");
+    logFile.close();
+}
+
+void tworzeniepliku1(const string name, const int count, const char value)
+{
+    fstream f;
+    f.open(name.c_str(), ios::binary | ios::out);
+    for (int i = 0; i < count; i++)
+    {
+        f.write((char*)&value, 1);
+    }
+    f.close();
+}
+
+void tworzenie()
+{
+    Otwarcie_loga("log.txt");
+
+    Zapis_Logow("Brak danych od uzytkownika");
+    Zapis_Logow("Tworzenie testowych plikow");
+    //test 1
+    tworzeniepliku1("plik1.1.bin", 100, 0x55);
+    tworzeniepliku1("plik1.2.bin", 100, 0x55);
+    //test 2
+
+    tworzeniepliku1("plik2.1.bin", 100, 0x55);
+    tworzeniepliku1("plik2.2.bin", 100, 0x48);
+
+    //test 3
+    tworzeniepliku1("plik3.1.bin", 419430400, 0x55);
+    tworzeniepliku1("plik3.2.bin", 419430400, 0x50);
+
+    Zapis_Logow("Testowe pliki sa tworzone");
+}
+
+
 
 int main(int argc, char* argv[])
 {
     if (argc != 3) {
+        tworzenie();
         do {
-            cout << "Kalkulator 0.1\n";
+            cout << "Kalkulator BER 0.2\n";
             cout << "1. Obliczenie BER" << endl << "2. Test 1" << endl << "3. Test 2" << endl << "4. Test 3" << endl << "0. Quit\n";
             
-            cin >> choice;
+            cin >> wybor;
 
-            switch (choice)
+            switch (wybor)
             {
             case 0:
                 cout << "Quit\n";
@@ -30,10 +100,9 @@ int main(int argc, char* argv[])
                 break;
             case 4:
                 cout << "Test3\n";
-                break;;
+                break;
             }
         } 
-        while (choice !=0);
+        while (wybor!=0);
     }
 }
-
